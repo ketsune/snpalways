3b2bc8858c883ac9eec6b11b2bac7c8ee8b3136c
Supanat Reanthong <supanat.reanthong@gmail.com>
2025-10-04 16:02:36 +0700

    first commit

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      // All requests starting with '/api' go to the Elysia server
+      '/api': {
+        target: 'http://localhost:3000', // Your Elysia port
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
