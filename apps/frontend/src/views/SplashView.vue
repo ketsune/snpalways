1d0c36ec0e235ad7c0877453b98673babbfccbd3
Supanat Reanthong <supanat.reanthong@gmail.com>
2026-01-20 23:34:18 +0700

    fix: attempt to fix tailwind @apply bug

--- apps/frontend/src/views/SplashView.vue
+++ apps/frontend/src/views/SplashView.vue
@@ -49,7 +49,7 @@ import EnvelopeAnimation from '@/components/EnvelopeAnimation.vue'
 }
 
 .brush-container:before {
-  @apply translate-x-[50%] md:translate-0;
+  @apply translate-x-[50%];
 
   content: '';
   display: inline-block;
@@ -72,6 +72,12 @@ import EnvelopeAnimation from '@/components/EnvelopeAnimation.vue'
   left: 0;
   position: absolute;
   animation: float 30s ease-in-out infinite;
+}
+
+@media (width >= 48rem) {
+  .brush-container:before {
+    @apply translate-0;
+  }
 }
 
 @keyframes float {
